63c9809076cda6cb876c7503495335439d88f1e5
Anto Julardzija <antjul95@gmail.com>
2018-12-06 08:26:47 +0100

    Home

--- src/hr/java/jule/test/Main.java
+++ src/hr/java/jule/test/Main.java
@@ -2,11 +2,14 @@ package hr.java.jule.test;
 
 import java.util.Scanner;
 
+import org.jfugue.player.Player;
 import org.jgap.Chromosome;
 import org.jgap.Configuration;
 import org.jgap.FitnessFunction;
 import org.jgap.Gene;
+import org.jgap.Genotype;
 import org.jgap.InvalidConfigurationException;
+import org.jgap.Population;
 import org.jgap.impl.DefaultConfiguration;
 import org.jgap.impl.StringGene;
 
@@ -14,31 +17,36 @@ public class Main {
 	
 	public static final int VELICINA_POPULACIJE=500;
 	
-	public static void algorithmSetup(int brojZnakova) throws InvalidConfigurationException {
+	public static Configuration algorithmSetup(String notes) throws InvalidConfigurationException {
 		
 		Configuration conf= new DefaultConfiguration();
-		StringGene[] sampleGenes= new StringGene[brojZnakova];
-		for(int i=0;i<brojZnakova;i++) {
-			sampleGenes[i]=new StringGene(conf, 1, 1);
+		StringGene[] sampleGenes= new StringGene[notes.length()];
+		for(int i=0;i<notes.length();i++) {
+			sampleGenes[i]=new StringGene(conf, 1, 1,"CDEFGAH ");
 		}
 		
 		Chromosome sampleChromosome= new Chromosome(conf, sampleGenes);
 		conf.setSampleChromosome(sampleChromosome);
 		conf.setPopulationSize(VELICINA_POPULACIJE);
 		
-		conf.setFitnessFunction(new MyFitnessFunction());
+		conf.setFitnessFunction(new MyFitnessFunction(notes));
+		return conf;
 	}
 	
 
 	public static void main(String[] args) throws InvalidConfigurationException {
-		
 		Scanner scanner = new Scanner(System.in);
 		String note;
 		
 		System.out.println("Upisite note:");
 		note=scanner.nextLine();
 		
-		algorithmSetup(note.length());
+		Configuration algorithmSetup=algorithmSetup(note);
+		Genotype population=Genotype.randomInitialGenotype(algorithmSetup);
+		for(int i=0;i<10;i++) {
+			System.out.println(population.getFittestChromosome());
+			population.evolve();
+		}
 		
 	}
 }
--- src/hr/java/jule/test/MyFitnessFunction.java
+++ src/hr/java/jule/test/MyFitnessFunction.java
@@ -4,11 +4,28 @@ import org.jgap.FitnessFunction;
 import org.jgap.IChromosome;
 
 public class MyFitnessFunction extends FitnessFunction {
+	
+	private String notes;
+	
+	public MyFitnessFunction(String stringNotes) {
+		// TODO Auto-generated constructor stub
+		this.notes=stringNotes;
+	}
+	
+	private int countErrors(IChromosome a_subject) {
+		int errors=0;
+		for(int i=0;i<this.notes.length();i++) {
+			if(!(Character.toString(notes.charAt(i)).equals(a_subject.getGene(i).getAllele()))) {
+				errors++;
+			}
+		}
+		return errors;
+	}
 
 	@Override
 	protected double evaluate(IChromosome a_subject) {
 		
-		return 0;
+		return 1.0/(countErrors(a_subject)+1);
 	}
 
 }
